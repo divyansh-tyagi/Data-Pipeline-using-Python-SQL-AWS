4db14e263e586d6a826bbfcaff0b41ebc5a15801
divyansh-tyagi <dtdivyansh1302@gmail.com>
2026-03-25 02:52:49 +0530

    transformed the previously cleaned data

--- main.py
+++ main.py
@@ -14,3 +14,20 @@ df["rating"] = df["rating"].fillna(df["rating"].mode()[0]) #used most frequent v
 df["date_added"] = pd.to_datetime(df["date_added"], errors="coerce") #convert date added to date time(TEXT-> PROPER DATE FORMAT)
 
 print("Cleaning done")
+
+print(df["type"].value_counts())
+
+df["content_type"] = df["type"]
+
+df["year_added"] = df["date_added"].dt.year 
+
+top_countries = df["country"].value_counts().head(5)
+print(top_countries)
+
+content_per_year = df["year_added"].value_counts().sort_index()
+print(content_per_year)
+
+avg_release = df.groupby("type")["release_year"].mean()
+print(avg_release)
+
+df.to_csv("netflix_cleaned.csv", index=False)
